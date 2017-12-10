c763e314561090edee1ee2bb17adbff591c62d22
mhmtdemir <mistikani.emir@gmail.com>
2017-12-11 01:35:07 +0300

    HAVA DURUMU KISMINI YAPTIM openweather api kullandım gelen bilgileri parse ettim icon kodları ,bilgileri filan lat longa göre aldıgım ıcın bılgılerı buyuk ıhtımal paralı anlamadım 3 günlük hava durumu filan alamadım hep paralıydı baska api yerlere pek bakmadım direk buraya başladım bu seferde dil kısmını türkçe yapamadım lang=tr yapıyorum ama yemiyor :DD bende bir kaç durum için switch case yaptım :D İcon kısımları pek hosuma gitmedi kodu incelersin başka api veren bir yer varsa daha detaylı 3 günlük filan onuda yaparım BİRKAÇ TA BUG VARDI TARLA EKLE HARİTA KISMINDA HANİ HARİTAYI İLK AÇIŞTA KAPANIYORDU ONU HALLETTİM

---
+++ app/src/main/java/com/example/harun/myflower/HavaDurumu/modelHava.java
@@ -0,0 +1,19 @@
+package com.example.harun.myflower.HavaDurumu;
+
+/**
+ * Created by mayne on 10.12.2017.
+ */
+
+public class modelHava {
+
+    //@SerializedName("weather")
+    public String coord;
+    public String weather;
+    //@SerializedName("main")
+    public String main;
+    //@SerializedName("name")
+    public String name;
+
+
+
+}
